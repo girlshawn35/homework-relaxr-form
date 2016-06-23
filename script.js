2bf6498bb250de25ca69997b95c121e1a0a15767
girlshawn35 <GirlShawn35@MacBook-Pro-2.local>
2016-06-23 16:07:01 -0700

    created, adds css styling

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	console.log("loaded")
+
+var select = document.getElementById ("selectState");
+
+var states = ["AK","AL","AR","AZ","CA","CO","CT","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"];
+	$("select").append("<option"+currentItem+"</option").val();
+
+}) 
